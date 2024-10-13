96f18be0d73f0166ee70700ab8beada6a037f935
fouad gabr 17 <foadjabr14@gmail.com>
2024-10-14 00:17:16 +0300

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Booking-and-reservation-project/",
+  base: "/Booking-and-reservation-system/",
 });

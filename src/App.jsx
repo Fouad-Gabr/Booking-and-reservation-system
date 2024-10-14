14702fc15c023e8494dde0ef707877a9041d34a4
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2024-10-14 14:58:44 +0300

    Implemented Home component with responsive design, added images, navigation links, and Font Awesome icons. Updated file path.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home"; // تأكد من مسار Home
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
-          <Route path="/businesses" element={<div>For Businesses Page</div>} />
-          <Route path="/login" element={<div>Login Page</div>} />
-          <Route path="/signup" element={<div>Sign Up Page</div>} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/businesses" element={<div>For Businesses Page</div>} />
+        <Route path="/login" element={<div>Login Page</div>} />
+        <Route path="/signup" element={<div>Sign Up Page</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

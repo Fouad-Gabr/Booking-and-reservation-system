254684c7e27f17cc480e96417c18ad7a71a15549
Karem Gobran <112480829+Karemgobran@users.noreply.github.com>
2024-10-14 13:51:48 +0300

    "Integrate Navbar with React Router: - Converted anchor tags to Link components for navigation. - Updated App.js to include BrowserRouter and Routes. - Added placeholder routes for Home, For Businesses, Login, and Sign Up pages."

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/businesses" element={<div>For Businesses Page</div>} />
+          <Route path="/login" element={<div>Login Page</div>} />
+          <Route path="/signup" element={<div>Sign Up Page</div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,88 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass, faBars } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light text-md-center sticky-top">
+        <div className="container">
+          <Link className="navbar-brand ps-3" to="/">
+            <img
+              src={logo}
+              alt="Logo"
+              width="35"
+              height="45"
+              className="w-100"
+            />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <form
+              action="#"
+              className="ms-3 d-flex position-relative flex-grow-1 flex-lg-grow-0 mb-3 mb-lg-0"
+            >
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                className="position-absolute"
+              />
+              <input
+                className="form-control ps-5 btn-outline-none mt-2 mt-md-0"
+                type="search"
+                placeholder="Search for a service or venue"
+                aria-label="Search"
+              />
+            </form>
+            <div className="bangalore ps-3 d-none d-lg-block">
+              <Link to="#">in Bangalore</Link>
+            </div>
+
+            <ul className="navbar-nav ms-lg-auto ps-3 mb-2 mb-lg-0 gap-md-3 gap-lg-4">
+              <li className="nav-item">
+                <Link className="nav-link" to="/businesses">
+                  For Businesses
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/signup">
+                  Sign Up
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="#">
+                  <FontAwesomeIcon
+                    icon={faBars}
+                    className="fs-3 d-none d-lg-block"
+                  />
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
